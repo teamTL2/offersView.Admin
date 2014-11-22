24b73100453a8e58855711aeae4a02ee19c2ca7a
3280amanatidis <wolfmen04@hotmail.com>
2014-11-22 02:51:18 +0200

    Classer of data storage

---
+++ src/register/Gps.java
@@ -0,0 +1,25 @@
+package register;
+
+
+public class Gps {
+    private double _X ;
+    private double _Y ;
+
+    public double getX() {
+        return _X;
+    }
+
+    public double getY() {
+        return _Y;
+    }
+
+
+    public void setX(float X) {
+        this._X = X;
+    }
+
+    public void setY(float Y) {
+        this._Y = Y;
+    }
+    
+}
